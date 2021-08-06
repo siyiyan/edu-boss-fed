244a4742cc8fffa2262433578bf89be85a0b732d
siyiyan <2964412698@qq.com>
2021-08-06 20:21:30 +0800

    角色权限管理

---
+++ src/views/role/components/CreateOrEdit.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="role">
+    <el-form>
+      <el-form-item label="角色名称">
+        <el-input v-model="role.name"></el-input>
+      </el-form-item>
+      <el-form-item label="角色编码">
+        <el-input v-model="role.code"></el-input>
+      </el-form-item>
+      <el-form-item label="角色描述">
+        <el-input v-model="role.description"></el-input>
+      </el-form-item>
+      <el-button>取消</el-button>
+      <el-button type="primary">确认</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { createOrUpdate } from '@/services/role'
+
+export default Vue.extend({
+  name: 'CreateOrEditRole',
+  data () {
+    return {
+      role: {
+        code: '',
+        name: '',
+        description: ''
+      }
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/role/components/List.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="role">
+    <div class="role-list">
+      <el-dialog
+        title="添加角色"
+        :visible.sync="dialogVisible"
+        width="30%">
+        <createOrEdit />
+      </el-dialog>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import CreateOrEdit from './CreateOrEdit.vue'
+
+export default Vue.extend({
+  name: 'RoleIndex',
+  components: {
+    CreateOrEdit
+  },
+  data () {
+    return {
+      dialogVisible: true // 控制添加/编辑角色的对话框显示和隐藏
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
